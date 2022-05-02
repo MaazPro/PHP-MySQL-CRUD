df853a47089f1ff356ec7cff5ad77daa062653ae
MaazPro <maazmuhammad94@gmail.com>
2022-05-02 18:34:58 +0800

    Displaying data in the table

--- process.php
+++ process.php
@@ -3,11 +3,23 @@
 $mysqli = new mysqli('localhost','root','','crud') or die (mysqli_error($mysqli)); 
 
 if(isset($_POST['save'])){
+    
     $name = $_POST['name'];
     $location = $_POST['location'];
     
-    $mysqli->query("INSERT INTO data (name, location) VALUES ('$name','$location')") 
-    or die($mysqli->error);
+    if(empty($name)){
+        echo "Name field is empty";
+    }
+    else if (empty($location)){
+        echo "Location field is empty";
+    }else{
+
+        $mysqli->query("INSERT INTO data (name, location) VALUES ('$name','$location')") or
+        die($mysqli->error);
+        header("location: index.php");
+        exit;
+    }
+    
 }
 
 ?>
